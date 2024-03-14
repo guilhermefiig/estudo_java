d2737ba9839adeb709d1018365c1ffdd83f36d9a
Guilherme Figueirêdo <guilherme.figueiredosc@gmail.com>
2024-03-14 16:43:02 -0300

    termino da atividade da seção 9

--- Seção 9 - Construtores, this, sobrecarga, encapsulamento/aula_81_exercicio/src/Programa.java
+++ Seção 9 - Construtores, this, sobrecarga, encapsulamento/aula_81_exercicio/src/Programa.java
@@ -5,6 +5,7 @@ public class Programa {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         Random random = new Random();
+        Banco banco = null;
 
         double depositoInicial = 0;
 
@@ -16,25 +17,21 @@ public class Programa {
         System.out.println("Você quer fazer um depósito inicial? [s/n]");
         char resposta = input.nextLine().charAt(0);
 
-        switch (resposta){
-            case 's':
-                System.out.print("Digite um valor para o depósito inicial: ");
-                depositoInicial =  input.nextDouble();
-                break;
+        if (resposta == 's') {
+            System.out.print("Digite um valor para o depósito inicial: ");
+            depositoInicial = input.nextDouble();
 
-            case 'n':
-                System.out.println();
-                break;
+            banco = new Banco(numeroConta, nome, depositoInicial);
 
-            default:
+        } else if (resposta == 'n') {
+            System.out.println();
+
+            banco = new Banco(numeroConta, nome);
+
+        }else{
                 System.out.printf("ERRO! %c não é reconhecido",resposta);
                 System.exit(0);
-                break;
-
         }
-
-        Banco banco = new Banco(numeroConta, nome);
-        banco.setSaldo(depositoInicial);
 
 
         System.out.println(banco.toString());
